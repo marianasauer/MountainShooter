fe2a7f01694fc8dceb460d0cb7f8110578336478
marianasauer <marianasauer2004@gmail.com>
2025-02-24 20:25:04 -0300

    Adding classes generated from StarUML

---
+++ code/Player.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Entity import Entity
+
+class Player(Entity):
+    def __init__(self):
+        pass
+
+    def update(self, ):
+        pass
+
+    def move(self, ):
+        pass
