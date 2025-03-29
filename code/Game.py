5d8f9c5eca7dbdc60e27fd5dcb16ca6e0a6f5520
marianasauer <marianasauer2004@gmail.com>
2025-03-29 13:04:50 -0300

    Level2 implementation

--- code/Game.py
+++ code/Game.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import pygame
 
 from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
@@ -10,7 +10,7 @@ from code.Menu import Menu
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(WIN_WIDTH , WIN_HEIGHT))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self, ):
         while True:
@@ -18,13 +18,15 @@ class Game:
             menu_return = menu.run()
 
             if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
-                level = Level(self.window, 'Level1', menu_return)
-                level_return = level.run()
+                player_score = [0, 0]  # [Player1, Player2]
+                level = Level(self.window, 'Level1', menu_return, player_score)
+                level_return = level.run(player_score)
+                if level_return:
+                    level = Level(self.window, 'Level2', menu_return, player_score)
+                    level_return = level.run()
+
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit()  # Close Window
                 quit()  # end pygame
             else:
                 pass
-
-
-
